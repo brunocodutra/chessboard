8433a355934807b0fb9e98a665643113e45463b4
Bruno Dutra <brunocodutra@gmail.com>
2020-05-10 23:38:05 +0200

    define enum InvalidPlayerAction

--- src/action.rs
+++ src/action.rs
@@ -1,4 +1,5 @@
-use crate::chess::{Piece, Square};
+use crate::chess::{Figure, Outcome, Piece, Player, Square};
+use thiserror::Error;
 
 /// Denotes a move.
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
@@ -17,4 +18,32 @@ pub enum PlayerAction {
 
     /// Resign the match in favor of the opponent.
     Resign(Player),
+}
+
+impl PlayerAction {
+    pub fn player(&self) -> &Player {
+        use PlayerAction::*;
+        match self {
+            MakeMove(p, _) | Resign(p) => p,
+        }
+    }
+}
+
+/// The reason why a player action was rejected.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Error)]
+pub enum InvalidPlayerAction {
+    #[error("the game has ended in a {}", .0)]
+    GameHasEnded(Outcome),
+
+    #[error("it's not {} player's turn", .0.color.to_str())]
+    TurnOfTheOpponent(Player),
+
+    #[error("the {} player is not allowed move the {} {} from {} to {} with {} promotion", 
+        .0.color.to_str(), .1.color.to_str(), .1.piece.to_str(), .2.from, .2.to,
+        .2.promotion.map(|p| p.to_str()).unwrap_or("no"))]
+    IllegalMove(Player, Figure, Move),
+
+    #[error("the {} player attempted to move a nonexistent piece from {} to {}", 
+        .0.color.to_str(),.1.from, .1.to)]
+    InvalidMove(Player, Move),
 }
