0375949df31496a4b04f7477c48882385de3ea4a
Bruno Dutra <brunocodutra@gmail.com>
2020-05-14 10:18:49 +0200

    implement basic game rules of standard chess

--- src/rules.rs
+++ src/rules.rs
@@ -1,1 +1,7 @@
-pub trait ChessRules {}
+use crate::action::{InvalidPlayerAction, PlayerAction};
+
+pub mod standard;
+
+pub trait ChessRules {
+    fn execute(&mut self, action: PlayerAction) -> Result<(), InvalidPlayerAction>;
+}
