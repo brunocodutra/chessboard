812414bd0fe1315d096f13ea6b12fb6dbbcba134
Bruno Dutra <brunocodutra@gmail.com>
2020-05-31 15:16:24 +0200

    impl FromStr for Promotion

--- src/promotion.rs
+++ src/promotion.rs
@@ -1,5 +1,6 @@
 use crate::{foreign, Piece};
-use derive_more::Display;
+use derive_more::{Display, Error};
+use std::str::FromStr;
 
 /// A chess piece.
 #[derive(Debug, Display, Copy, Clone, Eq, PartialEq, Hash)]
@@ -13,6 +14,32 @@ pub enum Promotion {
     Rook,
     #[display(fmt = "q")]
     Queen,
+}
+
+/// The reason why a player action was rejected.
+#[derive(Debug, Display, Copy, Clone, Eq, PartialEq, Hash, Error)]
+#[cfg_attr(test, derive(proptest_derive::Arbitrary))]
+#[display(
+    fmt = "unable to parse promotion, expected one of four characters: '{}', '{}', '{}', '{}'",
+    "Promotion::Knight",
+    "Promotion::Bishop",
+    "Promotion::Rook",
+    "Promotion::Queen"
+)]
+pub struct ParsePromotionError;
+
+impl FromStr for Promotion {
+    type Err = ParsePromotionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "n" => Ok(Promotion::Knight),
+            "b" => Ok(Promotion::Bishop),
+            "r" => Ok(Promotion::Rook),
+            "q" => Ok(Promotion::Queen),
+            _ => Err(ParsePromotionError),
+        }
+    }
 }
 
 impl Into<Piece> for Promotion {
@@ -35,6 +62,24 @@ impl Into<foreign::Piece> for Promotion {
             Bishop => foreign::Piece::Bishop,
             Rook => foreign::Piece::Rook,
             Queen => foreign::Piece::Queen,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use proptest::prelude::*;
+
+    proptest! {
+        #[test]
+        fn parsing_printed_promotion_is_an_identity(p: Promotion) {
+            assert_eq!(p.to_string().parse(), Ok(p));
+        }
+
+        #[test]
+        fn parsing_promotion_fails_except_for_one_of_four_letters(p in "[^nbrq]*") {
+            assert_eq!(p.parse::<Promotion>(), Err(ParsePromotionError));
         }
     }
 }
