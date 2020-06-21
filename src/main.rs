6e67bd12bd5b9d120350d89aba0856e1d673b372
Bruno Dutra <brunocodutra@gmail.com>
2020-06-21 20:42:19 +0200

    add cli argument to control the number of matches

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::{bail, Context, Error as Anyhow};
 use chessboard::*;
 use clap::AppSettings::*;
-use futures::try_join;
-use std::{cmp::min, error::Error, io::stderr};
+use futures::{prelude::*, stream::iter, try_join};
+use std::{cmp::min, collections::BTreeMap, error::Error, io::stderr, num::NonZeroUsize};
 use structopt::StructOpt;
 use tracing::*;
 use url::Url;
@@ -33,7 +33,7 @@ async fn new_player(color: Color, url: &Url) -> Result<ActorDispatcher<RemoteDis
 
 #[instrument(skip(white, black), err)]
 #[allow(clippy::unit_arg)]
-async fn chessboard(white: &Url, black: &Url) -> Result<(), Anyhow> {
+async fn chessboard(white: &Url, black: &Url) -> Result<Outcome, Anyhow> {
     let mut game = Game::new();
 
     let (mut white, mut black) = try_join!(
@@ -65,7 +65,7 @@ async fn chessboard(white: &Url, black: &Url) -> Result<(), Anyhow> {
 
     info!(%outcome);
 
-    Ok(())
+    Ok(outcome)
 }
 
 #[derive(StructOpt)]
@@ -90,6 +90,15 @@ struct AppSpec {
     black: Url,
 
     #[structopt(
+        short = "n",
+        long,
+        value_name = "n",
+        default_value = "1",
+        parse(try_from_str)
+    )]
+    best_of: NonZeroUsize,
+
+    #[structopt(
         short,
         long,
         value_name = "level",
@@ -97,6 +106,12 @@ struct AppSpec {
         parse(try_from_str)
     )]
     verbosity: Level,
+}
+
+macro_rules! echo {
+    ($($arg:tt)*) => ({
+        async_std::io::stdout().write_all(format!($($arg)*).as_bytes()).await
+    })
 }
 
 fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
@@ -115,7 +130,28 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
         .with_env_filter(filter)
         .try_init()?;
 
-    smol::run(chessboard(&spec.white, &spec.black)).context("the match was interrupted")?;
+    smol::run(async {
+        let stats = iter(0..spec.best_of.get())
+            .map(Ok)
+            .and_then(|_| chessboard(&spec.white, &spec.black))
+            .try_fold(BTreeMap::<_, usize>::new(), |mut acc, o| async move {
+                *acc.entry(o.to_string()).or_default() += 1;
+                Ok(acc)
+            })
+            .await
+            .context("the match was interrupted")?;
 
-    Ok(())
+        let width = (spec.best_of.get() as f64).log10().ceil() as usize + 1;
+
+        echo!("+{:-<w$}+\n", "", w = width + 44)?;
+        echo!("| {:<w$} |\n", "Statistics", w = width + 42)?;
+        echo!("+{:-<w$}+\n", "", w = width + 44)?;
+        for (key, abs) in stats {
+            let rel = (100 * abs) / spec.best_of.get();
+            echo!("| {:<31} | {:>w$} | {:>3} % |\n", key, abs, rel, w = width)?;
+        }
+        echo!("+{:-<w$}+\n", "", w = width + 44)?;
+
+        Ok(())
+    })
 }
