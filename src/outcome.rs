159268d7d06fc3d4d48d7bfce33c2b72d0bc2273
Bruno Dutra <brunocodutra@gmail.com>
2022-06-05 14:12:06 +0200

    introduce fns is_draw, is_decisive, and winner to Outcome

--- src/outcome.rs
+++ src/outcome.rs
@@ -17,3 +17,59 @@ pub enum Outcome {
     #[display(fmt = "draw by insufficient material")]
     DrawByInsufficientMaterial,
 }
+
+impl Outcome {
+    /// Whether the outcome is a [draw] and neither side has won.
+    ///
+    /// [draw]: https://en.wikipedia.org/wiki/Glossary_of_chess#draw
+    pub fn is_draw(&self) -> bool {
+        use Outcome::*;
+        matches!(self, Stalemate | DrawByInsufficientMaterial)
+    }
+
+    /// Whether the outcome is a decisive and one of the sides has won.
+    pub fn is_decisive(&self) -> bool {
+        use Outcome::*;
+        matches!(self, Resignation(_) | Checkmate(_))
+    }
+
+    /// The winning side, if the outcome is [decisive](`is_decisive`).
+    pub fn winner(&self) -> Option<Color> {
+        match *self {
+            Outcome::Checkmate(c) => Some(c),
+            Outcome::Resignation(c) => Some(!c),
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_strategy::proptest;
+
+    #[proptest]
+    fn outcome_is_either_draw_or_decisive(o: Outcome) {
+        assert_ne!(o.is_draw(), o.is_decisive());
+    }
+
+    #[proptest]
+    fn neither_side_wins_if_draw(#[filter(#o.is_draw())] o: Outcome) {
+        assert_eq!(o.winner(), None);
+    }
+
+    #[proptest]
+    fn one_side_wins_if_decisive(#[filter(#o.is_decisive())] o: Outcome) {
+        assert_ne!(o.winner(), None);
+    }
+
+    #[proptest]
+    fn side_that_checkmates_wins(c: Color) {
+        assert_eq!(Outcome::Checkmate(c).winner(), Some(c));
+    }
+
+    #[proptest]
+    fn side_that_resigns_loses(c: Color) {
+        assert_eq!(Outcome::Resignation(c).winner(), Some(!c));
+    }
+}
