5fc11bb63b2e5c80727dbbf7be44b5d834a0f908
Bruno Dutra <brunocodutra@gmail.com>
2022-06-21 19:31:38 +0200

    standardize newtypes for Shakmaty as tuple structs

--- src/position.rs
+++ src/position.rs
@@ -14,7 +14,7 @@ use proptest::{prelude::*, sample::Selector};
 #[cfg_attr(test, derive(test_strategy::Arbitrary))]
 #[debug(fmt = "Position(\"{}\")", self)]
 #[display(fmt = "{}", "Fen::from(self.clone())")]
-pub struct Position {
+pub struct Position(
     #[cfg_attr(test, strategy((0..32, any::<Selector>()).prop_map(|(depth, selector)| {
         let mut chess = sm::Chess::default();
         for _ in 0..depth {
@@ -26,32 +26,32 @@ pub struct Position {
         }
         chess
     })))]
-    pub(crate) board: sm::Chess,
-}
+    sm::Chess,
+);
 
 impl Position {
     /// The side to move.
     pub fn turn(&self) -> Color {
-        sm::Position::turn(&self.board).into()
+        sm::Position::turn(&self.0).into()
     }
 
     /// The number of halfmoves since the last capture or pawn advance.
     ///
     /// It resets to 0 whenever a piece is captured or a pawn is moved.
     pub fn halfmoves(&self) -> u32 {
-        sm::Position::halfmoves(&self.board)
+        sm::Position::halfmoves(&self.0)
     }
 
     /// The current move number since the start of the game.
     ///
     /// It starts at 1, and is incremented after every move by black.
     pub fn fullmoves(&self) -> NonZeroU32 {
-        sm::Position::fullmoves(&self.board)
+        sm::Position::fullmoves(&self.0)
     }
 
     /// The [`Square`]s occupied by [`Piece`]s of a kind.
     pub fn pieces(&self, p: Piece) -> impl ExactSizeIterator<Item = Square> {
-        sm::Position::board(&self.board)
+        sm::Position::board(&self.0)
             .by_piece(p.into())
             .into_iter()
             .map(Square::from)
@@ -59,7 +59,7 @@ impl Position {
 
     /// Into where the piece in this [`Square`] can attack.
     pub fn attacks(&self, s: Square) -> impl ExactSizeIterator<Item = Square> {
-        sm::Position::board(&self.board)
+        sm::Position::board(&self.0)
             .attacks_from(s.into())
             .into_iter()
             .map(Square::from)
@@ -67,7 +67,7 @@ impl Position {
 
     /// From where pieces of this [`Color`] can attack into this [`Square`].
     pub fn attackers(&self, s: Square, c: Color) -> impl ExactSizeIterator<Item = Square> {
-        let board = sm::Position::board(&self.board);
+        let board = sm::Position::board(&self.0);
         board
             .attacks_to(s.into(), c.into(), board.occupied())
             .into_iter()
@@ -76,23 +76,23 @@ impl Position {
 
     /// The [`Square`]s occupied by [`Piece`]s giving check.
     pub fn checkers(&self) -> impl ExactSizeIterator<Item = Square> {
-        sm::Position::checkers(&self.board)
+        sm::Position::checkers(&self.0)
             .into_iter()
             .map(Square::from)
     }
 
     // An iterator over the legal [`Move`]s that can be played in this position.
     pub fn moves(&self) -> impl ExactSizeIterator<Item = Move> {
-        sm::Position::legal_moves(&self.board)
+        sm::Position::legal_moves(&self.0)
             .into_iter()
             .map(|m| sm::uci::Uci::from_standard(&m).into())
     }
 
     /// Play a [`Move`] if legal in this position.
     pub fn play(&mut self, m: Move) -> Result<San, IllegalMove> {
-        match sm::uci::Uci::to_move(&m.into(), &self.board) {
-            Ok(vm) if sm::Position::is_legal(&self.board, &vm) => {
-                Ok(sm::san::SanPlus::from_move_and_play_unchecked(&mut self.board, &vm).into())
+        match sm::uci::Uci::to_move(&m.into(), &self.0) {
+            Ok(vm) if sm::Position::is_legal(&self.0, &vm) => {
+                Ok(sm::san::SanPlus::from_move_and_play_unchecked(&mut self.0, &vm).into())
             }
 
             _ => Err(IllegalMove(m, self.clone())),
@@ -106,7 +106,7 @@ impl Position {
 #[allow(clippy::derive_hash_xor_eq)]
 impl Hash for Position {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        state.write_u128(sm::zobrist::ZobristHash::zobrist_hash(&self.board));
+        state.write_u128(sm::zobrist::ZobristHash::zobrist_hash(&self.0));
     }
 }
 
@@ -117,7 +117,7 @@ impl Index<Square> for Position {
     fn index(&self, s: Square) -> &Self::Output {
         use Color::*;
         use Role::*;
-        match sm::Position::board(&self.board)
+        match sm::Position::board(&self.0)
             .piece_at(s.into())
             .map(Into::into)
         {
@@ -184,30 +184,44 @@ impl TryFrom<Fen> for Position {
     type Error = IllegalPosition;
 
     fn try_from(fen: Fen) -> Result<Self, Self::Error> {
-        Ok(Position {
-            board: sm::Setup::from(fen).position(sm::CastlingMode::Standard)?,
-        })
+        Ok(Position(
+            sm::Setup::from(fen).position(sm::CastlingMode::Standard)?,
+        ))
     }
 }
 
 #[doc(hidden)]
 impl From<Position> for sm::Setup {
     fn from(pos: Position) -> Self {
-        sm::Position::into_setup(pos.board, sm::EnPassantMode::Always)
+        sm::Position::into_setup(pos.0, sm::EnPassantMode::Always)
     }
 }
 
 #[doc(hidden)]
 impl From<sm::Chess> for Position {
     fn from(chess: sm::Chess) -> Self {
-        Position { board: chess }
+        Position(chess)
     }
 }
 
 #[doc(hidden)]
 impl From<Position> for sm::Chess {
     fn from(pos: Position) -> Self {
-        pos.board
+        pos.0
+    }
+}
+
+#[doc(hidden)]
+impl AsRef<sm::Chess> for Position {
+    fn as_ref(&self) -> &sm::Chess {
+        &self.0
+    }
+}
+
+#[doc(hidden)]
+impl AsMut<sm::Chess> for Position {
+    fn as_mut(&mut self) -> &mut sm::Chess {
+        &mut self.0
     }
 }
 
@@ -280,7 +294,7 @@ mod tests {
 
     #[proptest]
     fn moves_returns_the_legal_moves_from_this_position(pos: Position) {
-        let moves: Vec<Move> = sm::Position::legal_moves(&pos.board)
+        let moves: Vec<Move> = sm::Position::legal_moves(&pos.0)
             .iter()
             .map(sm::uci::Uci::from_standard)
             .map(Into::into)
@@ -296,9 +310,9 @@ mod tests {
         mut pos: Position,
         #[strategy(select(#pos.moves().collect::<Vec<_>>()))] m: Move,
     ) {
-        let vm = sm::uci::Uci::to_move(&m.into(), &pos.board)?;
-        let san = sm::san::SanPlus::from_move(pos.board.clone(), &vm).into();
-        let after = sm::Position::play(pos.board.clone(), &vm)?.into();
+        let vm = sm::uci::Uci::to_move(&m.into(), &pos.0)?;
+        let san = sm::san::SanPlus::from_move(pos.0.clone(), &vm).into();
+        let after = sm::Position::play(pos.0.clone(), &vm)?.into();
         assert_eq!(pos.play(m), Ok(san));
         assert_eq!(pos, after);
     }
@@ -317,7 +331,7 @@ mod tests {
     fn position_can_be_indexed_by_square(pos: Position, s: Square) {
         assert_eq!(
             pos[s],
-            sm::Position::board(&pos.board)
+            sm::Position::board(&pos.0)
                 .piece_at(s.into())
                 .map(Into::into)
         );
