eee69cc08a0ebb5a95dfa6b9b103b23e6d368076
Bruno Dutra <brunocodutra@gmail.com>
2022-06-23 00:02:11 +0200

    introduce trait Binary

---
+++ src/binary.rs
@@ -0,0 +1,16 @@
+use crate::Register;
+
+/// Trait for types that can be encoded to binary.
+pub trait Binary: Sized {
+    /// A fixed width collection of bits.
+    type Register: Register;
+
+    /// The reason why decoding failed.
+    type Error;
+
+    /// Encodes `Self` to its binary representation.
+    fn encode(&self) -> Self::Register;
+
+    /// Decodes `Self` from its binary representation.
+    fn decode(register: Self::Register) -> Result<Self, Self::Error>;
+}
