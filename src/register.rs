232e5af18a9df097e3745551193f7161d73a69a4
Bruno Dutra <brunocodutra@gmail.com>
2022-06-26 18:42:48 +0200

    impl Register for all T: BitStore + BitRegister

--- src/register.rs
+++ src/register.rs
@@ -1,5 +1,11 @@
+use bitvec::{mem::BitRegister, store::BitStore};
+
 /// Trait for fixed width collection of bits.
 pub trait Register: Copy {
     /// How many bits this register contains.
     const WIDTH: usize;
+}
+
+impl<T: BitStore + BitRegister> Register for T {
+    const WIDTH: usize = T::BITS as usize;
 }
