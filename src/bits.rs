8a9f52e8fe0f27253563f067d7bf9bb770a8a39b
Bruno Dutra <brunocodutra@gmail.com>
2022-06-28 22:53:07 +0200

    impl Binary for Action

--- src/bits.rs
+++ src/bits.rs
@@ -14,11 +14,19 @@ use proptest::prelude::*;
 #[display(fmt = "{:b}", "self.deref()")]
 #[repr(transparent)]
 pub struct Bits<T: BitStore + BitRegister, const W: usize>(
-    #[cfg_attr(test, strategy(*args..=T::ALL >> (T::BITS as usize - W)))] T,
+    #[cfg_attr(test, strategy(*args..=!(T::ALL << W)))] T,
 );
 
 impl<T: BitStore + BitRegister, const W: usize> Register for Bits<T, W> {
     const WIDTH: usize = W;
+}
+
+impl<T: BitStore + BitRegister, const W: usize> Bits<T, W> {
+    /// The largest possible value.
+    #[inline]
+    pub fn max() -> Self {
+        Bits(!(T::ALL << W))
+    }
 }
 
 /// Constructs [`Bits`] from any [`BitSlice`].
@@ -57,6 +65,11 @@ mod tests {
     use test_strategy::proptest;
 
     #[proptest]
+    fn max_has_all_bits_set_to_one() {
+        assert_eq!(Bits::<u16, 12>::max().count_ones(), 12);
+    }
+
+    #[proptest]
     fn can_be_converted_from_wider_sequence_of_raw_bits(b: Bits<u32, 12>) {
         assert_eq!(Bits::<u16, 12>::from(&*b).load::<u32>(), b.load::<u32>());
     }
@@ -70,11 +83,5 @@ mod tests {
     #[proptest]
     fn bits_has_fixed_width(b: Bits<u16, 12>) {
         assert_eq!(b.len(), 12);
-    }
-
-    #[proptest]
-    fn bits_can_be_mutated(mut b: Bits<u16, 12>) {
-        b.fill(true);
-        assert_eq!(b.load::<u32>(), (1 << 12) - 1);
     }
 }
