2b81dd8fc348db7d188253fd46ff472088aa6e14
Bruno Dutra <brunocodutra@gmail.com>
2022-06-30 00:00:28 +0200

    simplify Cache to take length as usize

--- src/cache.rs
+++ src/cache.rs
@@ -1,7 +1,8 @@
 use crate::Binary;
 use atomic::{Atomic, Ordering};
-use std::{error::Error, num::NonZeroUsize};
+use std::error::Error;
 
+#[derive(Debug)]
 /// A fixed-size concurrent in-memory cache.
 pub struct Cache<T: Default + Binary> {
     memory: Vec<Atomic<T::Register>>,
@@ -13,10 +14,10 @@ where
     T::Error: Error,
 {
     /// Constructs a [`Cache`] with `size` many slots filled with `T::default()`.
-    pub fn new(size: NonZeroUsize) -> Self {
+    pub fn new(size: usize) -> Self {
         debug_assert!(Atomic::<T::Register>::is_lock_free());
         let bits = T::default().encode();
-        let memory = (0..size.get()).map(|_| Atomic::new(bits)).collect();
+        let memory = (0..size).map(|_| Atomic::new(bits)).collect();
         Cache { memory }
     }
 
@@ -55,8 +56,8 @@ mod tests {
     use test_strategy::proptest;
 
     #[proptest]
-    fn new_initializes_cache(#[strategy(1..=100usize)] s: usize, #[strategy(0..#s)] i: usize) {
-        let cache = Cache::<Bits<u64, 48>>::new(s.try_into()?);
+    fn new_initializes_cache(#[strategy(0..=100usize)] s: usize, #[strategy(0..#s)] i: usize) {
+        let cache = Cache::<Bits<u64, 48>>::new(s);
         assert_eq!(
             cache.memory[i].load(Ordering::SeqCst),
             Bits::<u64, 48>::default()
@@ -65,7 +66,7 @@ mod tests {
 
     #[proptest]
     fn load_reads_value_at_index(#[strategy(1..=100usize)] s: usize, #[strategy(0..#s)] i: usize) {
-        let cache = Cache::<Bits<u64, 48>>::new(s.try_into()?);
+        let cache = Cache::<Bits<u64, 48>>::new(s);
         assert_eq!(cache.load(i), cache.memory[i].load(Ordering::SeqCst));
     }
 
@@ -75,7 +76,7 @@ mod tests {
         #[strategy(0..#s)] i: usize,
         v: Bits<u64, 48>,
     ) {
-        let cache = Cache::<Bits<u64, 48>>::new(s.try_into()?);
+        let cache = Cache::<Bits<u64, 48>>::new(s);
         cache.store(i, v);
         assert_eq!(cache.memory[i].load(Ordering::SeqCst), v);
     }
@@ -86,7 +87,7 @@ mod tests {
         #[strategy(0..#s)] i: usize,
         v: Bits<u64, 48>,
     ) {
-        let cache = Cache::<Bits<u64, 48>>::new(s.try_into()?);
+        let cache = Cache::<Bits<u64, 48>>::new(s);
         cache.update(i, |_| Some(v));
         assert_eq!(cache.memory[i].load(Ordering::SeqCst), v);
     }
@@ -96,7 +97,7 @@ mod tests {
         #[strategy(1..=100usize)] s: usize,
         #[strategy(0..#s)] i: usize,
     ) {
-        let cache = Cache::<Bits<u64, 48>>::new(s.try_into()?);
+        let cache = Cache::<Bits<u64, 48>>::new(s);
         cache.update(i, |_| None);
         assert_eq!(
             cache.memory[i].load(Ordering::SeqCst),
