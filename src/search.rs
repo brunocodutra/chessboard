a8cb55906994104c913bc37e3954eda8f67684f3
Bruno Dutra <brunocodutra@gmail.com>
2022-07-03 17:20:30 +0200

    replace SearchConfig by NegamaxConfig

--- src/search.rs
+++ src/search.rs
@@ -1,15 +1,8 @@
 use crate::{Action, Game};
 
-#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Hash)]
-pub struct SearchControl {
-    pub depth: Option<u8>,
-}
-
 /// Trait for types that implement adversarial search algorithms.
 #[cfg_attr(test, mockall::automock)]
 pub trait Search {
     /// Searches for the strongest [`Action`], if one exists.
-    ///
-    /// Implementors are expected to respect the limits specified in `ctrl`.
-    fn search(&self, game: &Game, ctrl: SearchControl) -> Option<Action>;
+    fn search(&self, game: &Game) -> Option<Action>;
 }
