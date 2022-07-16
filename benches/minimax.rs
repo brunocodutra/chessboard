0748ece74095aa62b33bb4aa53f7264783384943
Bruno Dutra <brunocodutra@gmail.com>
2022-07-16 15:20:01 +0200

    update minimax benchmark to simulate real game

--- benches/minimax.rs
+++ benches/minimax.rs
@@ -1,31 +1,33 @@
 use chessboard::{strategy::Minimax, Engine, Game, Search};
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
-use proptest::{prelude::*, sample::Selector, strategy::ValueTree, test_runner::TestRunner};
-
-fn game() -> impl Strategy<Value = Game> {
-    any::<Selector>().prop_map(|selector| {
-        let mut game = Game::default();
-        for _ in 0..32 {
-            if let Some(m) = selector.try_select(game.position().moves()) {
-                game.execute(m.into()).unwrap();
-            } else {
-                break;
-            }
-        }
-        game
-    })
-}
+use criterion::measurement::{Measurement, WallTime};
+use criterion::{criterion_group, criterion_main, Criterion, SamplingMode};
+use std::time::Duration;
 
 fn bench(c: &mut Criterion) {
-    let mut runner = TestRunner::default();
-    let minimax = Minimax::new(Engine::default());
-    c.bench_function("minimax", |b| {
-        b.iter_batched_ref(
-            || game().new_tree(&mut runner).unwrap().current(),
-            |game| minimax.search(game),
-            BatchSize::SmallInput,
-        )
-    });
+    c.benchmark_group("benches")
+        .sample_size(10)
+        .sampling_mode(SamplingMode::Flat)
+        .warm_up_time(Duration::from_secs(3))
+        .measurement_time(Duration::from_secs(30))
+        .bench_function("minimax", |b| {
+            b.iter_custom(|iters| {
+                let mut game = Game::default();
+                let minimax = Minimax::new(Engine::default());
+
+                let timer = WallTime.start();
+
+                for i in 0..iters {
+                    match minimax.search(&game) {
+                        None => return WallTime.end(timer).mul_f64(iters as f64 / i as f64),
+                        Some(a) => {
+                            game.execute(a).unwrap();
+                        }
+                    }
+                }
+
+                WallTime.end(timer)
+            })
+        });
 }
 
 criterion_group!(benches, bench);
