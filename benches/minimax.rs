f5a6964fe333ef1e37471fac1c96413def82d968
Bruno Dutra <brunocodutra@gmail.com>
2022-07-31 14:10:03 +0200

    improve benchmark stability

--- benches/minimax.rs
+++ benches/minimax.rs
@@ -1,3 +1,4 @@
+use chessboard::strategy::MinimaxConfig;
 use chessboard::{strategy::Minimax, Engine, Position, Search};
 use criterion::measurement::{Measurement, WallTime};
 use criterion::{criterion_group, criterion_main, Criterion, SamplingMode};
@@ -5,14 +6,20 @@ use std::time::Duration;
 
 fn bench(c: &mut Criterion) {
     c.benchmark_group("benches")
-        .sample_size(10)
+        .sample_size(50)
         .sampling_mode(SamplingMode::Flat)
         .warm_up_time(Duration::from_secs(3))
-        .measurement_time(Duration::from_secs(30))
+        .measurement_time(Duration::from_secs(50))
         .bench_function("minimax", |b| {
             b.iter_custom(|iters| {
                 let mut pos = Position::default();
-                let minimax = Minimax::new(Engine::default());
+                let minimax = Minimax::with_config(
+                    Engine::default(),
+                    MinimaxConfig {
+                        max_depth: 5,
+                        ..MinimaxConfig::default()
+                    },
+                );
 
                 let timer = WallTime.start();
 
