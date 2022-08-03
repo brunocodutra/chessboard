75e93395572586858566abc996103b32e678b4ae
Bruno Dutra <brunocodutra@gmail.com>
2022-08-03 20:31:53 +0200

    add support for multi game matches

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::{Context, Error as Anyhow};
-use chessboard::{Build, Game, PlayerBuilder};
+use chessboard::{Build, Color, Game, PlayerBuilder};
 use clap::{AppSettings::DeriveDisplayOrder, Parser};
-use std::{cmp::min, io::stderr};
+use libm::erf;
+use std::{cmp::min, io::stderr, num::NonZeroUsize};
 use tracing::{info, Level};
 use tracing_subscriber::fmt::format::FmtSpan;
 
@@ -9,12 +10,16 @@ use tracing_subscriber::fmt::format::FmtSpan;
 #[clap(author, version, about, name = "Chessboard", setting = DeriveDisplayOrder)]
 struct Args {
     /// White pieces player.
-    #[clap(short, long, default_value = "cli()", parse(try_from_str))]
+    #[clap(short, long, value_name = "player", default_value = "cli()")]
     white: PlayerBuilder,
 
     /// Black pieces player.
-    #[clap(short, long, default_value = "cli()", parse(try_from_str))]
+    #[clap(short, long, value_name = "player", default_value = "cli()")]
     black: PlayerBuilder,
+
+    /// How many games to play.
+    #[clap(short = 'n', long, value_name = "number", default_value = "1")]
+    games: NonZeroUsize,
 
     /// Verbosity level.
     #[clap(short, long, value_name = "level", parse(try_from_str))]
@@ -28,6 +33,7 @@ async fn main() -> Result<(), Anyhow> {
     let Args {
         white,
         black,
+        games,
         verbosity,
     } = Args::parse();
 
@@ -43,10 +49,35 @@ async fn main() -> Result<(), Anyhow> {
         .map_err(|e| Anyhow::msg(e.to_string()))
         .context("failed to initialize the tracing infrastructure")?;
 
-    let mut game = Game::default();
-    let report = game.run(white.build()?, black.build()?).await?;
-    info!(outcome = %report.outcome);
-    println!("{}", report);
+    let (mut wins, mut losses, mut draws) = (0f64, 0f64, 0f64);
+    let mut reports = Vec::with_capacity(games.into());
+
+    for n in 0..games.into() {
+        let white = white.clone().build()?;
+        let black = black.clone().build()?;
+        let report = Game::default().run(white, black).await?;
+        info!(outcome = %report.outcome);
+
+        match report.outcome.winner() {
+            Some(Color::White) => wins += 1.,
+            Some(Color::Black) => losses += 1.,
+            None => draws += 1.,
+        }
+
+        info!(
+            games = n + 1,
+            white = wins + draws / 2.,
+            black = losses + draws / 2.,
+            ΔELO = -400. * ((wins + losses + draws) / (wins + draws / 2.) - 1.).log10(),
+            LOS = (1. + erf((wins - losses) / (2. * (wins + losses)).sqrt())) / 2.
+        );
+
+        reports.push(report);
+    }
+
+    for report in reports {
+        println!("{}\n", report);
+    }
 
     Ok(())
 }
