32f2364f30b9fb2f1afb58a3dc757989152d20d1
Bruno Dutra <brunocodutra@gmail.com>
2022-08-17 23:08:10 +0200

    make benchmark results more stable

--- benches/minimax.rs
+++ benches/minimax.rs
@@ -1,45 +1,26 @@
 use chessboard::strategy::{Minimax, MinimaxConfig};
 use chessboard::{Engine, Position, Search, SearchLimits};
-use criterion::measurement::{Measurement, WallTime};
-use criterion::{criterion_group, criterion_main, Criterion, SamplingMode};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use std::time::Duration;
 
 fn bench(c: &mut Criterion) {
-    c.benchmark_group("benches")
-        .sample_size(50)
-        .sampling_mode(SamplingMode::Flat)
-        .warm_up_time(Duration::from_secs(3))
-        .measurement_time(Duration::from_secs(50))
-        .bench_function("minimax", |b| {
-            b.iter_custom(|iters| {
-                let mut pos = Position::default();
-                let limits = SearchLimits {
-                    time: Duration::MAX,
-                    depth: 5,
-                };
+    let limits = SearchLimits {
+        time: Duration::MAX,
+        depth: 5,
+    };
 
-                let minimax = Minimax::with_config(
-                    Engine::default(),
-                    MinimaxConfig {
-                        search: limits,
-                        ..MinimaxConfig::default()
-                    },
-                );
+    let config = MinimaxConfig {
+        search: limits,
+        ..MinimaxConfig::default()
+    };
 
-                let timer = WallTime.start();
-
-                for i in 0..iters {
-                    match minimax.search(&pos).next() {
-                        None => return WallTime.end(timer).mul_f64(iters as f64 / i as f64),
-                        Some(t) => {
-                            pos.make(t.best()).unwrap();
-                        }
-                    }
-                }
-
-                WallTime.end(timer)
-            })
-        });
+    c.benchmark_group("benches").bench_function("minimax", |b| {
+        b.iter_batched_ref(
+            || Minimax::with_config(Engine::default(), config),
+            |s| s.search(&Position::default()).next(),
+            BatchSize::SmallInput,
+        );
+    });
 }
 
 criterion_group!(benches, bench);
