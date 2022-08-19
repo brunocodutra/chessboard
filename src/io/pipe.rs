37ac3c08f127681e50b51ae87b93a4f17d96c3dc
Bruno Dutra <brunocodutra@gmail.com>
2022-08-19 21:51:21 +0200

    split struct Pipe off of Process

---
+++ src/io/pipe.rs
@@ -0,0 +1,91 @@
+use crate::Io;
+use async_trait::async_trait;
+use std::io;
+use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader, Lines};
+use tracing::instrument;
+
+/// A generic [`Io`] interface.
+#[derive(Debug)]
+pub struct Pipe<W: AsyncWrite, R: AsyncRead> {
+    writer: W,
+    reader: Lines<BufReader<R>>,
+}
+
+impl<W: AsyncWrite, R: AsyncRead> Pipe<W, R> {
+    pub fn new(writer: W, reader: R) -> Self {
+        Pipe {
+            writer,
+            reader: BufReader::new(reader).lines(),
+        }
+    }
+}
+
+impl<W: AsyncWrite, R: AsyncRead> From<(W, R)> for Pipe<W, R> {
+    fn from((writer, reader): (W, R)) -> Self {
+        Pipe::new(writer, reader)
+    }
+}
+
+#[async_trait]
+impl<W: AsyncWrite + Send + Unpin, R: AsyncRead + Send + Unpin> Io for Pipe<W, R> {
+    #[instrument(level = "trace", skip(self), ret, err)]
+    async fn recv(&mut self) -> io::Result<String> {
+        use io::ErrorKind::UnexpectedEof;
+        Ok(self.reader.next_line().await?.ok_or(UnexpectedEof)?)
+    }
+
+    #[instrument(level = "trace", skip(self), err)]
+    async fn send(&mut self, msg: &str) -> io::Result<()> {
+        self.writer.write_all(msg.as_bytes()).await?;
+        self.writer.write_u8(b'\n').await?;
+        Ok(())
+    }
+
+    #[instrument(level = "trace", skip(self), err)]
+    async fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush().await?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str;
+    use test_strategy::proptest;
+    use tokio::io::{duplex, AsyncReadExt, BufReader};
+    use tokio::runtime;
+
+    #[proptest]
+    fn recv_waits_for_line_break(#[strategy("[^\r\n]")] s: String) {
+        let rt = runtime::Builder::new_multi_thread().build()?;
+
+        let (stdin, _) = duplex(1);
+        let (mut tx, stdout) = duplex(s.len() + 1);
+
+        rt.block_on(tx.write_all(s.as_bytes()))?;
+        rt.block_on(tx.write_u8(b'\n'))?;
+
+        let mut pipe = Pipe::new(stdin, BufReader::new(stdout));
+        assert_eq!(rt.block_on(pipe.recv())?, s);
+    }
+
+    #[proptest]
+    fn send_appends_line_break(s: String) {
+        let rt = runtime::Builder::new_multi_thread().build()?;
+
+        let (stdin, mut rx) = duplex(s.len() + 1);
+        let (_, stdout) = duplex(1);
+
+        let expected = format!("{}\n", s);
+
+        let mut pipe = Pipe::new(stdin, BufReader::new(stdout));
+        rt.block_on(pipe.send(&s))?;
+        rt.block_on(pipe.flush())?;
+
+        let mut buf = vec![0u8; expected.len()];
+        rt.block_on(rx.read_exact(&mut buf))?;
+
+        assert_eq!(str::from_utf8(&buf)?, expected);
+    }
+}
--- src/io/process.rs
+++ src/io/process.rs
@@ -1,100 +1,69 @@
-use crate::Io;
+use crate::{Io, Pipe};
 use anyhow::{bail, Context, Error as Anyhow};
 use async_trait::async_trait;
 use std::{io, time::Duration};
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter, Lines};
 use tokio::{runtime, select, task::block_in_place, time::sleep};
 use tracing::{error, field::display, instrument, Span};
 
 #[async_trait]
-#[cfg_attr(test, mockall::automock(
-    type Stdin = tokio::io::DuplexStream;
-    type Stdout = tokio::io::DuplexStream;
-    type Status = String;
-))]
-trait ChildProcess {
-    type Stdin;
-    type Stdout;
-    fn pipe(&mut self) -> io::Result<(Self::Stdin, Self::Stdout)>;
-
-    type Status;
-    async fn wait(&mut self) -> io::Result<Self::Status>;
-
+#[cfg_attr(test, mockall::automock)]
+trait Child {
+    async fn wait(&mut self) -> io::Result<String>;
     async fn kill(&mut self) -> io::Result<()>;
 }
-
-#[async_trait]
-impl ChildProcess for tokio::process::Child {
-    type Stdin = tokio::process::ChildStdin;
-    type Stdout = tokio::process::ChildStdout;
-    fn pipe(&mut self) -> io::Result<(Self::Stdin, Self::Stdout)> {
-        Option::zip(self.stdin.take(), self.stdout.take()).ok_or_else(|| {
-            io::Error::new(
-                io::ErrorKind::Other,
-                Anyhow::msg("failed to open the remote process' stdio"),
-            )
-        })
-    }
-
-    type Status = std::process::ExitStatus;
-    async fn wait(&mut self) -> io::Result<Self::Status> {
-        self.wait().await
-    }
-
-    async fn kill(&mut self) -> io::Result<()> {
-        self.kill().await
-    }
-}
-
-#[cfg(test)]
-type Child = MockChildProcess;
-
-#[cfg(not(test))]
-type Child = tokio::process::Child;
 
 /// An [`Io`] interface for a remote process.
 #[derive(Debug)]
 pub struct Process {
-    child: Child,
-    writer: BufWriter<<Child as ChildProcess>::Stdin>,
-    reader: Lines<BufReader<<Child as ChildProcess>::Stdout>>,
+    #[cfg(test)]
+    pipe: Pipe<tokio::io::DuplexStream, tokio::io::DuplexStream>,
+
+    #[cfg(not(test))]
+    pipe: Pipe<tokio::process::ChildStdin, tokio::process::ChildStdout>,
+
+    #[cfg(test)]
+    child: MockChild,
+
+    #[cfg(not(test))]
+    child: tokio::process::Child,
 }
 
 impl Process {
     #[cfg(test)]
-    const TIMEOUT: Duration = Duration::from_millis(0);
+    const TIMEOUT: Duration = Duration::ZERO;
 
     #[cfg(not(test))]
     const TIMEOUT: Duration = Duration::from_millis(1000);
-
-    fn new(mut child: Child) -> io::Result<Self> {
-        let (stdin, stdout) = child.pipe()?;
-
-        Ok(Process {
-            child,
-            writer: BufWriter::new(stdin),
-            reader: BufReader::new(stdout).lines(),
-        })
-    }
 
     /// Spawns a remote process.
     #[instrument(level = "trace", err)]
     pub fn spawn(path: &str) -> io::Result<Self> {
         #[cfg(test)]
         {
-            let mut child = MockChildProcess::new();
-            child.expect_pipe().returning(|| Ok(tokio::io::duplex(1)));
-            Process::new(child)
+            Ok(Process {
+                pipe: tokio::io::duplex(1).into(),
+                child: MockChild::new(),
+            })
         }
 
         #[cfg(not(test))]
         {
-            Process::new(
-                tokio::process::Command::new(path)
-                    .stdin(std::process::Stdio::piped())
-                    .stdout(std::process::Stdio::piped())
-                    .spawn()?,
-            )
+            let mut child = tokio::process::Command::new(path)
+                .stdin(std::process::Stdio::piped())
+                .stdout(std::process::Stdio::piped())
+                .spawn()?;
+
+            let pipe = Option::zip(child.stdin.take(), child.stdout.take()).ok_or_else(|| {
+                io::Error::new(
+                    io::ErrorKind::Other,
+                    Anyhow::msg("failed to open the remote process' stdio"),
+                )
+            })?;
+
+            Ok(Process {
+                pipe: pipe.into(),
+                child,
+            })
         }
     }
 }
@@ -105,7 +74,7 @@ impl Drop for Process {
     fn drop(&mut self) {
         let result: Result<_, Anyhow> = block_in_place(|| {
             runtime::Handle::try_current()?.block_on(async {
-                self.writer.flush().await?;
+                self.flush().await?;
 
                 select! {
                     status = self.child.wait() => {
@@ -123,7 +92,7 @@ impl Drop for Process {
         match result.context("failed to gracefully terminate the remote process") {
             Err(e) => error!("{:?}", e),
             Ok(s) => {
-                Span::current().record("outcome", display(s));
+                Span::current().record("status", display(s));
             }
         }
     }
@@ -131,65 +100,37 @@ impl Drop for Process {
 
 #[async_trait]
 impl Io for Process {
-    #[instrument(level = "trace", skip(self), ret, err)]
     async fn recv(&mut self) -> io::Result<String> {
-        use io::ErrorKind::UnexpectedEof;
-        Ok(self.reader.next_line().await?.ok_or(UnexpectedEof)?)
+        self.pipe.recv().await
     }
 
-    #[instrument(level = "trace", skip(self), ret, err)]
     async fn send(&mut self, msg: &str) -> io::Result<()> {
-        self.writer.write_all(msg.as_bytes()).await?;
-        self.writer.write_u8(b'\n').await?;
-        Ok(())
+        self.pipe.send(msg).await
     }
 
-    #[instrument(level = "trace", skip(self), ret, err)]
     async fn flush(&mut self) -> io::Result<()> {
-        self.writer.flush().await?;
-        Ok(())
+        self.pipe.flush().await
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::str;
     use test_strategy::proptest;
-    use tokio::io::{duplex, AsyncReadExt};
-
-    #[proptest]
-    fn new_expects_stdin_and_stdout() {
-        let mut child = MockChildProcess::new();
-
-        let pipe = duplex(1);
-        child.expect_pipe().once().return_once(move || Ok(pipe));
-
-        assert!(Process::new(child).is_ok());
-    }
-
-    #[proptest]
-    fn new_fails_if_stdio_not_available(e: io::Error) {
-        let mut child = MockChildProcess::new();
-
-        let kind = e.kind();
-        child.expect_pipe().once().return_once(move || Err(e));
-
-        assert_eq!(Process::new(child).err().map(|e| e.kind()), Some(kind));
-    }
 
     #[proptest]
     fn drop_gracefully_terminates_child_process(status: String) {
         let rt = runtime::Builder::new_multi_thread().enable_time().build()?;
 
         let mut process = Process::spawn("")?;
-        let child = &mut process.child;
 
-        child
+        process
+            .child
             .expect_wait()
-            .return_once(move || Box::pin(async { Ok(status) }));
+            .return_once(move || Box::pin(async move { Ok(status) }));
 
-        child
+        process
+            .child
             .expect_kill()
             .return_once(move || Box::pin(async { Ok(()) }));
 
@@ -203,16 +144,16 @@ mod tests {
         let rt = runtime::Builder::new_multi_thread().enable_time().build()?;
 
         let mut process = Process::spawn("")?;
-        let child = &mut process.child;
 
-        child.expect_wait().return_once(move || {
-            Box::pin(async {
+        process.child.expect_wait().return_once(move || {
+            Box::pin(async move {
                 sleep(Duration::from_secs(1)).await;
                 Ok(status)
             })
         });
 
-        child
+        process
+            .child
             .expect_kill()
             .once()
             .return_once(move || Box::pin(async { Ok(()) }));
@@ -227,13 +168,14 @@ mod tests {
         let rt = runtime::Builder::new_multi_thread().enable_time().build()?;
 
         let mut process = Process::spawn("")?;
-        let child = &mut process.child;
 
-        child
+        process
+            .child
             .expect_wait()
             .return_once(move || Box::pin(async { Err(a) }));
 
-        child
+        process
+            .child
             .expect_kill()
             .return_once(move || Box::pin(async { Err(b) }));
 
@@ -245,44 +187,5 @@ mod tests {
     #[proptest]
     fn drop_recovers_from_missing_runtime() {
         drop(Process::spawn("")?);
-    }
-
-    #[proptest]
-    fn recv_waits_for_line_break(#[strategy("[^\r\n]")] s: String) {
-        let rt = runtime::Builder::new_multi_thread().build()?;
-        let mut child = MockChildProcess::new();
-
-        let (stdin, _) = duplex(1);
-        let (mut tx, stdout) = duplex(s.len() + 1);
-        let pipe = (stdin, stdout);
-        child.expect_pipe().once().return_once(move || Ok(pipe));
-
-        rt.block_on(tx.write_all(s.as_bytes()))?;
-        rt.block_on(tx.write_u8(b'\n'))?;
-
-        let mut process = Process::new(child)?;
-        assert_eq!(rt.block_on(process.recv())?, s);
-    }
-
-    #[proptest]
-    fn send_appends_line_break(s: String) {
-        let rt = runtime::Builder::new_multi_thread().build()?;
-        let mut child = MockChildProcess::new();
-
-        let (stdin, mut rx) = duplex(s.len() + 1);
-        let (_, stdout) = duplex(1);
-        let pipe = (stdin, stdout);
-        child.expect_pipe().once().return_once(move || Ok(pipe));
-
-        let expected = format!("{}\n", s);
-
-        let mut process = Process::new(child)?;
-        rt.block_on(process.send(&s))?;
-        rt.block_on(process.flush())?;
-
-        let mut buf = vec![0u8; expected.len()];
-        rt.block_on(rx.read_exact(&mut buf))?;
-
-        assert_eq!(str::from_utf8(&buf)?, expected);
     }
 }
