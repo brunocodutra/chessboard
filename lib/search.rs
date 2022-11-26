7629d16f44acad2653a8cb40973c9c7a6c579073
Bruno Dutra <brunocodutra@gmail.com>
2022-11-26 19:25:28 +0100

    remove some unnecessary complexity

--- lib/search.rs
+++ lib/search.rs
@@ -133,28 +133,14 @@ impl Searcher {
         }
     }
 
-    fn moves(
-        &self,
-        pos: &Position,
-        kind: MoveKind,
-        pv: Option<Move>,
-    ) -> Vec<(Move, Position, i16)> {
-        let mut moves: Vec<_> = pos
-            .moves(kind)
+    fn moves(&self, pos: &Position, kind: MoveKind) -> Vec<(Move, Position, i16)> {
+        pos.moves(kind)
             .map(|(m, next)| {
-                let value = if pv == Some(m) {
-                    i16::MAX
-                } else {
-                    let mut exchanges = next.exchanges(m.whither());
-                    -self.see(&next, &mut exchanges, -i16::MAX..i16::MAX)
-                };
-
+                let mut exchanges = next.exchanges(m.whither());
+                let value = -self.see(&next, &mut exchanges, -i16::MAX..i16::MAX);
                 (m, next, value)
             })
-            .collect();
-
-        moves.sort_unstable_by_key(|(_, _, value)| *value);
-        moves
+            .collect()
     }
 
     /// An implementation of [null move pruning].
@@ -309,12 +295,20 @@ impl Searcher {
             MoveKind::ANY
         };
 
-        let mut moves = self.moves(pos, kind, transposition.map(|t| t.best()));
+        let mut moves = self.moves(pos, kind);
+
+        moves.sort_unstable_by_key(|&(m, _, value)| {
+            if Some(m) == transposition.map(|t| t.best()) {
+                i16::MAX
+            } else {
+                value
+            }
+        });
 
         let (score, pv) = match moves.pop() {
             None => return Ok(Score(value, draft)),
-            Some((m, next, _)) => {
-                let score = -self.aw(&next, -value, -beta..-alpha, draft - 1, time, metrics)?;
+            Some((m, next, value)) => {
+                let score = -self.aw(&next, value, -beta..-alpha, draft - 1, time, metrics)?;
 
                 if *score >= beta {
                     metrics.pv_cut();
@@ -335,23 +329,17 @@ impl Searcher {
                 let mut score = Score(-i16::MAX, -i8::MAX);
                 let mut alpha = cutoff.load(Ordering::Relaxed);
 
-                if alpha >= beta {
-                    return Ok(None);
-                }
-
-                if !in_check {
-                    if let Some(d) = self.lmp(&next, value, alpha, draft) {
-                        if d < 0 || *-self.nw(&next, -alpha - 1, d, time, metrics)? < alpha {
-                            #[cfg(not(test))]
-                            // The late move pruning heuristic is not exact.
-                            return Ok(Some((score, m)));
-                        } else {
-                            alpha = cutoff.load(Ordering::Relaxed);
+                while *score < alpha && alpha < beta {
+                    if !in_check {
+                        if let Some(d) = self.lmp(&next, value, alpha, draft) {
+                            if d < 0 || *-self.nw(&next, -alpha - 1, d, time, metrics)? < alpha {
+                                #[cfg(not(test))]
+                                // The late move pruning heuristic is not exact.
+                                return Ok(Some((score, m)));
+                            }
                         }
                     }
-                }
 
-                while *score < alpha && alpha < beta {
                     score = -self.nw(&next, -alpha - 1, draft - 1, time, metrics)?;
 
                     if *score < alpha {
