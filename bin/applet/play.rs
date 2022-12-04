7a6740ad453817df254ee16f875f80b5cfbc993c
Bruno Dutra <brunocodutra@gmail.com>
2022-12-04 17:03:58 +0100

    improve match statistics with confidence interval

--- bin/applet/play.rs
+++ bin/applet/play.rs
@@ -1,8 +1,8 @@
 use crate::{engine::EngineConfig, game::Game};
 use anyhow::Error as Anyhow;
 use clap::Parser;
+use derive_more::{DebugCustom, Display};
 use lib::chess::Position;
-use libm::erf;
 use std::num::NonZeroUsize;
 use tracing::{info, instrument};
 
@@ -21,11 +21,36 @@ pub struct Play {
     defender: EngineConfig,
 }
 
+#[derive(DebugCustom, Display, Copy, Clone)]
+#[debug(fmt = "{}", self)]
+#[display(fmt = "[{:+.2}, {:+.2}]", _0, _1)]
+struct ΔELO(f64, f64);
+
+impl ΔELO {
+    fn new(wins: f64, losses: f64, draws: f64) -> Self {
+        let n = wins + losses + draws;
+        let w = wins / n;
+        let l = losses / n;
+        let d = draws / n;
+
+        let wvar = w * (l + d / 2.).powi(2);
+        let lvar = l * (w + d / 2.).powi(2);
+        let dvar = d * (w - l).powi(2);
+
+        let error = (wvar + lvar + dvar).sqrt() / n.sqrt();
+
+        ΔELO(
+            400. * ((w - 1.96 * error).max(0.) / (l + 1.96 * error)).log10(),
+            400. * ((w + 1.96 * error) / (l - 1.96 * error).max(0.)).log10(),
+        )
+    }
+}
+
 impl Play {
     #[instrument(level = "trace", skip(self), err)]
     pub async fn execute(self) -> Result<(), Anyhow> {
         let players = [self.challenger, self.defender];
-        let (mut wins, mut losses, mut draws) = (0f64, 0f64, 0f64);
+        let (mut wins, mut losses, mut draws) = (0, 0, 0);
         let mut pgns = Vec::with_capacity(self.games.into());
 
         for n in 0..self.games.into() {
@@ -34,16 +59,16 @@ impl Play {
 
             let wl = [&mut wins, &mut losses];
             match pgn.outcome.winner() {
-                Some(c) => *wl[(n + c as usize) % 2] += 1.,
-                _ => draws += 1.,
+                Some(c) => *wl[(n + c as usize) % 2] += 1,
+                _ => draws += 1,
             }
 
             info!(
-                games = n + 1,
-                challenger = wins + draws / 2.,
-                defender = losses + draws / 2.,
-                ΔELO = -400. * ((wins + losses + draws) / (wins + draws / 2.) - 1.).log10(),
-                LOS = (1. + erf((wins - losses) / (2. * (wins + losses)).sqrt())) / 2.
+                games = wins + losses + draws,
+                wins,
+                losses,
+                draws,
+                ΔELO = %ΔELO::new(wins as _, losses as _, draws as _),
             );
 
             pgns.push(pgn);
