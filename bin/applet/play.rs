de19df2b5f0923b1940cd03370dfd93ab891a688
Bruno Dutra <brunocodutra@gmail.com>
2022-12-08 23:26:21 +0100

    add support for playing multiple games in parallel

--- bin/applet/play.rs
+++ bin/applet/play.rs
@@ -2,23 +2,47 @@ use crate::{engine::EngineConfig, game::Game};
 use anyhow::Error as Anyhow;
 use clap::Parser;
 use derive_more::{DebugCustom, Display};
-use lib::chess::Position;
-use std::num::NonZeroUsize;
+use futures_util::future::try_join_all;
+use lib::chess::{Color, Position};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{iter::repeat, num::NonZeroUsize, sync::Arc};
+use tokio::{spawn, sync::Mutex};
 use tracing::{info, instrument};
 
 /// A match of chess between two players.
 #[derive(Debug, Parser)]
 #[clap(disable_help_flag = true, disable_version_flag = true)]
 pub struct Play {
-    /// How many games to play.
+    /// Total number of games to play.
     #[clap(short = 'n', long, default_value = "1")]
     games: NonZeroUsize,
 
-    /// The challenging player starts with the white pieces.
+    /// Maximum number of games to play in parallel.
+    #[clap(short = 'c', long, default_value = "1")]
+    concurrency: NonZeroUsize,
+
+    /// The challenging player.
     challenger: EngineConfig,
 
-    /// The defending player starts with the black pieces.
+    /// The defending player.
     defender: EngineConfig,
+}
+
+impl Play {
+    #[instrument(level = "trace", skip(self), err)]
+    pub async fn execute(self) -> Result<(), Anyhow> {
+        let players = [self.challenger, self.defender];
+        let games = Arc::new(AtomicUsize::new(self.games.into()));
+        let wld = Arc::new(Mutex::new([0; 3]));
+
+        let tasks = repeat((players, games, wld))
+            .take(self.concurrency.into())
+            .map(move |(players, games, wld)| spawn(play(players, games, wld)));
+
+        try_join_all(tasks).await?;
+
+        Ok(())
+    }
 }
 
 #[derive(DebugCustom, Display, Copy, Clone)]
@@ -27,11 +51,11 @@ pub struct Play {
 struct ΔELO(f64, f64);
 
 impl ΔELO {
-    fn new(wins: f64, losses: f64, draws: f64) -> Self {
-        let n = wins + losses + draws;
-        let w = wins / n;
-        let l = losses / n;
-        let d = draws / n;
+    fn new([wins, losses, draws]: [usize; 3]) -> Self {
+        let n = wins as f64 + losses as f64 + draws as f64;
+        let w = wins as f64 / n;
+        let l = losses as f64 / n;
+        let d = draws as f64 / n;
 
         let wvar = w * (l + d / 2.).powi(2);
         let lvar = l * (w + d / 2.).powi(2);
@@ -46,38 +70,36 @@ impl ΔELO {
     }
 }
 
-impl Play {
-    #[instrument(level = "trace", skip(self), err)]
-    pub async fn execute(self) -> Result<(), Anyhow> {
-        let players = [self.challenger, self.defender];
-        let (mut wins, mut losses, mut draws) = (0, 0, 0);
-        let mut pgns = Vec::with_capacity(self.games.into());
+#[instrument(level = "trace", skip(players, games, wld), err)]
+pub async fn play(
+    players: [EngineConfig; 2],
+    games: Arc<AtomicUsize>,
+    wld: Arc<Mutex<[usize; 3]>>,
+) -> Result<(), Anyhow> {
+    while let Ok(n) = games.fetch_update(Ordering::Relaxed, Ordering::Relaxed, |n| n.checked_sub(1))
+    {
+        let game = Game::new(players[(n - 1) % 2].clone(), players[n % 2].clone());
+        let pgn = game.play(Position::default()).await?;
 
-        for n in 0..self.games.into() {
-            let game = Game::new(players[n % 2].clone(), players[(n + 1) % 2].clone());
-            let pgn = game.play(Position::default()).await?;
-
-            let wl = [&mut wins, &mut losses];
+        {
+            let mut wld = wld.lock().await;
             match pgn.outcome.winner() {
-                Some(c) => *wl[(n + c as usize) % 2] += 1,
-                _ => draws += 1,
+                Some(Color::White) => wld[(n - 1) % 2] += 1,
+                Some(Color::Black) => wld[n % 2] += 1,
+                _ => wld[2] += 1,
             }
 
             info!(
-                games = wins + losses + draws,
-                wins,
-                losses,
-                draws,
-                ΔELO = %ΔELO::new(wins as _, losses as _, draws as _),
+                games = wld.iter().sum::<usize>(),
+                wins = wld[0],
+                losses = wld[1],
+                draws = wld[2],
+                ΔELO = %ΔELO::new(*wld),
             );
-
-            pgns.push(pgn);
         }
 
-        for pgn in pgns {
-            println!("{}\n", pgn);
-        }
-
-        Ok(())
+        println!("{}\n", pgn);
     }
+
+    Ok(())
 }
