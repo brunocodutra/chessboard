221b03a2df3385ac486809ea5ae0f653116a40d7
Bruno Dutra <brunocodutra@gmail.com>
2023-06-18 20:40:16 +0200

    impl Layer for struct Chain

---
+++ nnue/src/chain.rs
@@ -0,0 +1,15 @@
+use crate::Layer;
+use test_strategy::Arbitrary;
+
+/// The composition of two [`Layer`]s in series.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Arbitrary)]
+pub struct Chain<T, U>(pub T, pub U);
+
+impl<I, T: Layer<I>, U: Layer<T::Output>> Layer<I> for Chain<T, U> {
+    type Output = U::Output;
+
+    #[inline]
+    fn forward(&self, input: I) -> Self::Output {
+        self.1.forward(self.0.forward(input))
+    }
+}
