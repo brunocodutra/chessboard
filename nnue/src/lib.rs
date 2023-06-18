684e245dae93419bb92f3d43e08c195860510455
Bruno Dutra <brunocodutra@gmail.com>
2023-06-18 20:39:21 +0200

    introduce trait Layer

--- nnue/src/lib.rs
+++ nnue/src/lib.rs
@@ -1,3 +1,12 @@
 mod feature;
 
 use feature::*;
+
+/// Trait for types that can compose a neural network.
+trait Layer<Input> {
+    /// The transformed neurons.
+    type Output;
+
+    /// Transforms input neurons.
+    fn forward(&self, input: Input) -> Self::Output;
+}
