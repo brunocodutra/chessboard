ceb920d5a31273a82da44df8cc7634cc049ac01b
Bruno Dutra <brunocodutra@gmail.com>
2023-07-02 15:47:01 +0200

    introduce trait Transformer

--- nnue/src/lib.rs
+++ nnue/src/lib.rs
@@ -1,3 +1,5 @@
+use num_traits::PrimInt;
+
 mod affine;
 mod chain;
 mod crelu;
@@ -17,4 +19,30 @@ trait Layer<Input> {
 
     /// Transforms input neurons.
     fn forward(&self, input: Input) -> Self::Output;
+}
+
+/// Trait for types that can incrementally transform features.
+trait Transformer {
+    /// A type that can accumulate transformed features.
+    type Accumulator;
+
+    /// Refreshes accumulator.
+    fn refresh(&self, features: &[usize], accumulator: &mut Self::Accumulator);
+
+    /// Updates the accumulator by adding features.
+    fn add(&self, feature: usize, accumulator: &mut Self::Accumulator);
+
+    /// Updates the accumulator by removing features.
+    fn remove(&self, feature: usize, accumulator: &mut Self::Accumulator);
+}
+
+impl<T: Transformer<Accumulator = [U; N]>, U: PrimInt, const N: usize> Layer<&[usize]> for T {
+    type Output = [U; N];
+
+    #[inline]
+    fn forward(&self, input: &[usize]) -> Self::Output {
+        let mut accumulator = [U::zero(); N];
+        self.refresh(input, &mut accumulator);
+        accumulator
+    }
 }
