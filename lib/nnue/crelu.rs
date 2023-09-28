f773e93a9113a00724855809a8aeb9e2e6cc9f05
Bruno Dutra <brunocodutra@gmail.com>
2023-09-29 00:57:24 +0200

    manually unroll the affine matrix multiplication to facilitate vectorization

--- lib/nnue/crelu.rs
+++ lib/nnue/crelu.rs
@@ -1,5 +1,5 @@
 use crate::nnue::Layer;
-use num_traits::{AsPrimitive, PrimInt};
+use num_traits::AsPrimitive;
 
 /// A clipped [rectifier][ReLU].
 ///
@@ -11,25 +11,25 @@ pub struct CReLU<L>(pub(super) L);
 impl<L, I, T, const N: usize> Layer<I> for CReLU<L>
 where
     L: Layer<I, Output = [T; N]>,
-    T: PrimInt + AsPrimitive<i8> + From<i8>,
+    T: Ord + AsPrimitive<i8> + From<i8>,
 {
     type Output = [i8; N];
 
     fn forward(&self, input: I) -> Self::Output {
         self.0
             .forward(input)
-            .map(|v| v.clamp(T::zero(), i8::MAX.into()).as_())
+            .map(|v| v.clamp(0i8.into(), i8::MAX.into()).as_())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::nnue::Passthrough;
+    use crate::nnue::Input;
     use test_strategy::proptest;
 
     #[proptest]
-    fn clipped_relu_saturates_between_0_and_max(l: CReLU<Passthrough>, i: [i32; 3]) {
+    fn clipped_relu_saturates_between_0_and_max(l: CReLU<Input>, i: [i32; 3]) {
         assert_eq!(l.forward(i), i.map(|v| v.clamp(0, i8::MAX as _) as _));
     }
 }
