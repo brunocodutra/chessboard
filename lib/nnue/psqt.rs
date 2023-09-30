a958570293a32f6ecadee54de6e1eaa3a335bc6f
Bruno Dutra <brunocodutra@gmail.com>
2023-09-30 12:57:28 +0200

    manually unroll the PSQT transformer to facilitate vectorization

--- lib/nnue/psqt.rs
+++ lib/nnue/psqt.rs
@@ -12,8 +12,20 @@ impl<const I: usize, const O: usize> Transformer for Psqt<I, O> {
 
     fn refresh(&self, features: &[usize], accumulator: &mut [i32; O]) {
         *accumulator = [0; O];
+
         debug_assert!(features.len() <= 32);
-        for f in features.iter().take(32) {
+        let mut chunks = features.chunks_exact(4);
+
+        for f in &mut chunks {
+            for (i, a) in accumulator.iter_mut().enumerate() {
+                *a += self.0[f[0]][i];
+                *a += self.0[f[1]][i];
+                *a += self.0[f[2]][i];
+                *a += self.0[f[3]][i];
+            }
+        }
+
+        for f in chunks.remainder() {
             self.add(*f, accumulator)
         }
     }
