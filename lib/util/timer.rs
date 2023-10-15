22c39ad4cf9656d700e764e5703a11f6655b7f7e
Bruno Dutra <brunocodutra@gmail.com>
2023-10-15 13:51:47 +0200

    always search at least to depth 1

--- lib/util/timer.rs
+++ lib/util/timer.rs
@@ -5,20 +5,15 @@ use std::time::{Duration, Instant};
 #[display(fmt = "time is up!")]
 pub struct Timeout;
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+#[derive(Debug)]
 #[cfg_attr(test, derive(test_strategy::Arbitrary))]
 pub struct Timer {
     deadline: Option<Instant>,
 }
 
 impl Timer {
-    /// Constructs a timer that never elapses.
-    pub fn disarmed() -> Self {
-        Timer { deadline: None }
-    }
-
     /// Constructs a timer that elapses after the given duration.
-    pub fn start(duration: Duration) -> Self {
+    pub fn new(duration: Duration) -> Self {
         Timer {
             deadline: Instant::now().checked_add(duration),
         }
@@ -42,13 +37,13 @@ mod tests {
 
     #[proptest]
     fn timer_does_not_elapse_before_duration_expires() {
-        let timer = Timer::start(Duration::MAX);
+        let timer = Timer::new(Duration::MAX);
         assert_eq!(timer.elapsed(), Ok(()))
     }
 
     #[proptest]
     fn timer_elapses_once_duration_expires() {
-        let timer = Timer::start(Duration::ZERO);
+        let timer = Timer::new(Duration::ZERO);
         sleep(Duration::from_millis(1));
         assert_eq!(timer.elapsed(), Err(Timeout))
     }
