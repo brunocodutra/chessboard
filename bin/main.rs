4f3137fb5bc48ebc38659937dc859628458d9982
Bruno Dutra <brunocodutra@gmail.com>
2023-11-04 20:22:51 +0100

    remove dependency on crate clap

--- bin/main.rs
+++ bin/main.rs
@@ -1,22 +1,8 @@
-use clap::Parser;
 use lib::uci::Uci;
-use std::io;
-
-#[derive(Parser)]
-#[clap(author, version, about)]
-pub struct Cli {
-    #[arg(skip)]
-    server: Uci,
-}
-
-impl Cli {
-    fn run(mut self) -> io::Result<()> {
-        self.server.run()
-    }
-}
 
 fn main() {
-    if let Err(e) = Cli::parse().run() {
+    let mut server = Uci::default();
+    if let Err(e) = server.run() {
         panic!("{}", e);
     }
 }
