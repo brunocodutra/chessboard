dbbbd69754f080b27b6308da0209553e2c533c0a
Bruno Dutra <brunocodutra@gmail.com>
2023-11-23 00:52:01 +0100

    remove unnecessary struct Io

--- lib/util.rs
+++ lib/util.rs
@@ -4,7 +4,6 @@ mod bits;
 mod bounds;
 mod buffer;
 mod counter;
-mod io;
 mod saturating;
 mod timer;
 
@@ -14,6 +13,5 @@ pub use bits::*;
 pub use bounds::*;
 pub use buffer::*;
 pub use counter::*;
-pub use io::*;
 pub use saturating::*;
 pub use timer::*;
