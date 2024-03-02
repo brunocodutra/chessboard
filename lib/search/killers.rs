8ef282b0fe989b4a229fb9207fac2e13ce8943a3
Bruno Dutra <brunocodutra@gmail.com>
2024-03-02 17:13:29 +0100

    replace usage of []::rotate_right

--- lib/search/killers.rs
+++ lib/search/killers.rs
@@ -6,31 +6,29 @@ use crate::search::Ply;
 /// [killer moves]: https://www.chessprogramming.org/Killer_Move
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 #[cfg_attr(test, derive(test_strategy::Arbitrary))]
-pub struct Killers<const N: usize, const P: usize>([[[Option<Move>; N]; 2]; P]);
+pub struct Killers<const P: usize>([[[Option<Move>; 2]; 2]; P]);
 
-impl<const N: usize, const P: usize> Default for Killers<N, P> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<const N: usize, const P: usize> Killers<N, P> {
+impl<const P: usize> Killers<P> {
     /// Constructs an empty set of killer moves.
+    #[inline(always)]
     pub const fn new() -> Self {
-        Killers([[[None; N]; 2]; P])
+        Killers([[[None; 2]; 2]; P])
     }
 
     /// Adds a killer move to the set at a given ply for a given side to move.
+    #[inline(always)]
     pub fn insert(&mut self, ply: Ply, side: Color, m: Move) {
         if let Some(ks) = self.0.get_mut(ply.get() as usize) {
-            if !ks[side as usize].contains(&Some(m)) {
-                ks[side as usize].rotate_right(1);
-                ks[side as usize][0] = Some(m);
+            let [first, last] = &mut ks[side as usize];
+            if *first != Some(m) {
+                *last = *first;
+                *first = Some(m);
             }
         }
     }
 
     /// Checks whether move is a known killer at a given ply for a given side to move.
+    #[inline(always)]
     pub fn contains(&self, ply: Ply, side: Color, m: Move) -> bool {
         self.0
             .get(ply.get() as usize)
@@ -48,7 +46,7 @@ mod tests {
 
     #[proptest]
     fn insert_avoids_duplicated_moves(#[filter(#p >= 0)] p: Ply, c: Color, m: Move) {
-        let mut ks = Killers::<2, { Ply::MAX as usize + 1 }>::default();
+        let mut ks = Killers::<{ Ply::MAX as usize + 1 }>::new();
 
         ks.insert(p, c, m);
         ks.insert(p, c, m);
@@ -61,21 +59,21 @@ mod tests {
         #[filter(#p >= 0)] p: Ply,
         c: Color,
         #[any(size_range(2..10).lift())] ms: HashSet<Move>,
-        #[filter(!#ms.contains(&#m))] m: Move,
+        m: Move,
     ) {
-        let mut ks = Killers::<1, { Ply::MAX as usize + 1 }>::default();
+        let mut ks = Killers::<{ Ply::MAX as usize + 1 }>::new();
 
         for m in ms {
             ks.insert(p, c, m);
         }
 
         ks.insert(p, c, m);
-        assert_eq!(ks.0[p.get() as usize][c as usize], [Some(m)]);
+        assert_eq!(ks.0[p.get() as usize][c as usize][0], Some(m));
     }
 
     #[proptest]
     fn insert_ignores_ply_out_of_bounds(
-        mut ks: Killers<2, 1>,
+        mut ks: Killers<1>,
         #[filter(#p > 0)] p: Ply,
         c: Color,
         m: Move,
@@ -87,7 +85,7 @@ mod tests {
 
     #[proptest]
     fn contains_returns_true_only_if_inserted(#[filter(#p >= 0)] p: Ply, c: Color, m: Move) {
-        let mut ks = Killers::<2, { Ply::MAX as usize + 1 }>::default();
+        let mut ks = Killers::<{ Ply::MAX as usize + 1 }>::new();
         assert!(!ks.contains(p, c, m));
         ks.insert(p, c, m);
         assert!(ks.contains(p, c, m));
@@ -95,7 +93,7 @@ mod tests {
 
     #[proptest]
     fn contains_returns_false_if_ply_out_of_bounds(
-        ks: Killers<2, 1>,
+        ks: Killers<1>,
         #[filter(#p > 0)] p: Ply,
         c: Color,
         m: Move,
