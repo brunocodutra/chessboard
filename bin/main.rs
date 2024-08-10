4d32c7797a961e61f7c05639728ec613a5ab8e12
Bruno Dutra <brunocodutra@gmail.com>
2024-08-10 20:55:49 +0200

    avoid hanging reading from stdin on graceful shutdown

--- bin/main.rs
+++ bin/main.rs
@@ -5,34 +5,32 @@ use std::io::{prelude::*, stdin, stdout, LineWriter};
 use std::thread;
 
 fn main() {
-    thread::scope(|s| {
-        let (mut tx, input) = mpsc::channel(32);
-        let (output, rx) = mpsc::channel(32);
+    let (mut tx, input) = mpsc::channel(32);
+    let (output, rx) = mpsc::channel(32);
 
-        s.spawn(move || {
-            for item in stdin().lock().lines() {
-                match item {
-                    Err(error) => return eprint!("{error}"),
-                    Ok(line) => {
-                        if let Err(error) = block_on(tx.send(line)) {
-                            if error.is_disconnected() {
-                                break;
-                            }
+    thread::spawn(move || {
+        for item in stdin().lock().lines() {
+            match item {
+                Err(error) => return eprint!("{error}"),
+                Ok(line) => {
+                    if let Err(error) = block_on(tx.send(line)) {
+                        if error.is_disconnected() {
+                            break;
                         }
                     }
                 }
             }
-        });
-
-        s.spawn(move || {
-            let mut stdout = LineWriter::new(stdout().lock());
-            for line in block_on_stream(rx) {
-                if let Err(error) = writeln!(stdout, "{line}") {
-                    return eprint!("{error}");
-                }
-            }
-        });
-
-        block_on(Uci::new(input, output).run()).ok();
+        }
     });
+
+    thread::spawn(move || {
+        let mut stdout = LineWriter::new(stdout().lock());
+        for line in block_on_stream(rx) {
+            if let Err(error) = writeln!(stdout, "{line}") {
+                return eprint!("{error}");
+            }
+        }
+    });
+
+    block_on(Uci::new(input, output).run()).ok();
 }
